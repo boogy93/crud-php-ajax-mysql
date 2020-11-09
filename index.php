7a947438fb7e6bd92f517fd5401186814eeb6d7e
boogy93 <70883693+boogy93@users.noreply.github.com>
2020-11-09 12:03:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,41 @@
-<?php
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+       <link rel="stylesheet" href="style.scss"
+
+</head>
+<body>
+
+
+<!--Insertion de l'en-tete de notre site-->
+<?php ("Page Accueil"); ?>
+
+<!--EN-TETE DE PAGE-->
+
+<div id="nav">
+    <img src="img/psg.jpeg200x200">
+    <div class="btn">
+        <a href="login.php"><button href="login.php">Login</button></a>
+        <a href="newUser.php"><button id="login">Register</button></a>
+    </div>
+
+</div>
+<div id="txt">
+    <div>
+        Une page possède toujours une barre de navigation comme ci-dessus.
+        <ol>
+        1. Représente le logo de l’application
+        2. Représente le lien pour afficher la page de login
+        3. Représente le lien pour ouvrir une compte sur l’application
+        </ol>
+    </div>
+    </div>
+</div>
+</body>
+</html>
